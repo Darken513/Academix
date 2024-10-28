f9befdfc8787db3067489575aed82ec45308fab7
Darken <learningstuffs513@gmail.com>
2024-10-28 21:02:21 +0100

    nullable fields fix

--- Academix-SRV/src/models/centerReferedUser.ts
+++ Academix-SRV/src/models/centerReferedUser.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { Teacher } from './userRoles/Teacher';
 import { Cours } from './Cours';
 import { Student } from './userRoles/Student';
@@ -9,17 +9,17 @@ export class CenterReferedUser {
   @PrimaryGeneratedColumn()
   id!: number;  
 
-  @ManyToOne(() => Cours, (cours) => cours.centerReferedUsers)
-  @JoinColumn({name: 'cours_id'})
-  cours?: Cours;
+  @ManyToOne(() => Cours, (cours) => cours.centerReferedUsers, { nullable: false })
+  @JoinColumn({ name: 'cours_id' })
+  cours!: Cours;
 
-  @ManyToOne(() => Teacher, (teacher) => teacher.centerReferedUsers)
-  @JoinColumn({name: 'teacher_id'})
-  teacher?: Teacher;
+  @ManyToOne(() => Teacher, (teacher) => teacher.centerReferedUsers, { nullable: false })
+  @JoinColumn({ name: 'teacher_id' })
+  teacher!: Teacher;
 
-  @ManyToOne(() => Student, (student) => student.centerReferedUsers)
-  @JoinColumn({name: 'student_id'})
-  student?: Student;
+  @ManyToOne(() => Student, (student) => student.centerReferedUsers, { nullable: false })
+  @JoinColumn({ name: 'student_id' })
+  student!: Student;
 
   @Column({ type: 'boolean', default: true, nullable: true })
   enabled?: boolean;
