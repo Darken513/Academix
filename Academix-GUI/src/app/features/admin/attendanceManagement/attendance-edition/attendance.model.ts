599b10dd1d26e7520a59c2a3bf831ddf95f2cf98
Yacine Abid <yacineabid146@yahoo.fr>
2024-11-17 13:37:28 +0100

    some work within attendance
    Attendances edition section fields tested, submission works, verified with Postman.

--- Academix-GUI/src/app/features/admin/attendanceManagement/attendance-edition/attendance.model.ts
+++ Academix-GUI/src/app/features/admin/attendanceManagement/attendance-edition/attendance.model.ts
@@ -1,54 +1,57 @@
-import { FormEntity, FormField } from "../../../../shared/components/form-builder/utilities/FormEntity";
+import {
+  FormEntity,
+  FormField,
+} from '../../../../shared/components/form-builder/utilities/FormEntity';
 
 export class AttendanceForm extends FormEntity {
-    id: number | undefined;
+  id: number | undefined;
 
-    enabled: boolean | undefined;
-    last_update: Date | undefined;
-    created_at: Date | undefined;
-    @FormField({
-        label: 'Status',
-        type: 'text',
-        required: true,
-    })
-    status: string = '';
+  enabled: boolean | undefined;
+  last_update: Date | undefined;
+  created_at: Date | undefined;
+  @FormField({
+    label: 'Status',
+    type: 'radios',
+    options: ['present', 'absent'],
+    required: true,
+  })
+  status: string = '';
 
-    @FormField({
-        label: 'Notes',
-        type: 'text',
-        required: true,
-    })
-    notes: string = '';
+  @FormField({
+    label: 'Notes',
+    type: 'text',
+  })
+  notes: string = '';
 
-    @FormField({
-        label: 'Session',
-        type: 'select',
-        required: true,
-        options: [],
-        params: {
-            optionLabel: 'id',
-            returnKey: 'id',
-            hasFilter: true
-        },
-        fetchOptionsFrom: '/sessions/getAll'
-    })
-    session_id: string = '';
+  @FormField({
+    label: 'Session',
+    type: 'select',
+    required: true,
+    options: [],
+    params: {
+      optionLabel: 'id',
+      returnKey: 'id',
+      hasFilter: true,
+    },
+    fetchOptionsFrom: '/sessions/getAll',
+  })
+  session_id: string = '';
 
-    @FormField({
-        label: 'Student',
-        type: 'select',
-        required: true,
-        options: [],
-        params: {
-            optionLabel: 'name',
-            returnKey: 'id',
-            hasFilter: true
-        },
-        fetchOptionsFrom: '/students/getAll'
-    })
-    student_id: string = '';
-    
-    constructor() {
-        super();
-    }
+  @FormField({
+    label: 'Student',
+    type: 'select',
+    required: true,
+    options: [],
+    params: {
+      optionLabel: 'first_name',
+      returnKey: 'id',
+      hasFilter: true,
+    },
+    fetchOptionsFrom: '/students/getAll',
+  })
+  student_id: string = '';
+
+  constructor() {
+    super();
+  }
 }
--- Academix-GUI/src/app/features/admin/coursManagement/cours-edition/cours-edition.component.ts
+++ Academix-GUI/src/app/features/admin/coursManagement/cours-edition/cours-edition.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CoursForm } from './cours.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cours-edition',
@@ -8,7 +9,19 @@ import { CoursForm } from './cours.model';
 })
 export class CoursEditionComponent {
   entity: CoursForm = new CoursForm()
-  displayMap = undefined
+  isNew: boolean = true;
+  displayMap = [
+    ['subject_id', 'teacher_id'],
+    ['managed_by_center'],
+    ['studentPaymentType'],
+    ['student_price_per_session', 'student_price_flat_rate'],
+    ['teacherPaymentType'],
+    ['teacher_price_per_session', 'teacher_price_per_student', 'teacher_price_flat_rate'],
+    ['unpaid_total']
+  ]
+
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit(): void {
     //this.entity.parseJSON({ textareaRegex: 'description here, some long text', regexLimited: '123', password: 'HelloWorld', radioOptions: 'first value' })
@@ -17,8 +30,12 @@ export class CoursEditionComponent {
   }
 
   public onSubmit() {
-    //it should send the data to server and save cache version if succeeded
     console.log(this.entity);
+    this.http.post<any>('http://localhost:8080/courses/create', this.entity).subscribe({
+      next: (next) => {
+        console.log(next);
+      }
+    });
   }
 }
 
